1cf2aa9b5daa7aafc4749c28528297633af5e803
bouch <yassine.bouchoucha@cocentris.com>
2020-05-06 17:34:01 +0100

    add route to CRUDS

--- blog_post/models.py
+++ blog_post/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class UserPost( models.Model):
    title = models.TextField()
    contents=models.TextField(null=True,blank=True)
+
+   def get_absolute_url(self):
+      return f"/blog/{self.pk}/"
+
+   def get_edit_url(self):
+      return f"/blog/{self.pk}/edit/"
+
+   def get_delete_url(self):
+      return f"/blog/{self.pk}/delete/"
